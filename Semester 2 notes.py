4db393ce7d8c65abe7bb8030fcee37a2eaf1a659
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-01-08 09:46:13 -0800

    Semester 2 notes

---
+++ Semester 2 notes.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+print(" Fortnite is the best")
+
+# this is a single line comment
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+print("I Have " + str(cars) + "cars ")
+
+age = input("How old are you?")
+print(age + "??? You really shouldn't lie about your age.")
+
+colors = ["blue", "orange", "yellow", "red", "green"]
+colors.append("Magenta")
+
+print(colors)
+colors.pop(0)
+print(colors)
+print(len(colors))
+
+
