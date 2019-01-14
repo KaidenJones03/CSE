7df1816a8871a96901b5f012e26ce78db2c1676e
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-01-14 09:43:32 -0800

    Kaiden's Hangman

--- Kaiden's hangman.py
+++ Kaiden's hangman.py
@@ -1,6 +1,10 @@
+import random
 wordBank = ["Violin", "Banjo", "Guitar", "Ukulele", "Drums", "Piano", "Accordion", "Flute", "Clarinet", "Trumpet"]
 Guesses = 8
-
-print("Welcome to Hangman!")
-print("Pick a letter")
-
+word = random.choice(wordBank)
+guess = input
+print("Welcome to Hangman!") 
+guess(input)
+if guess =:false
+Guesses = -1
+else guess == true
