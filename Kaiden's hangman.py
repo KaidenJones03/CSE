d16660a495a26b2bd3726ec654e47950625745bf
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-01-16 09:45:15 -0800

    Kaiden's hangman

--- Kaiden's hangman.py
+++ Kaiden's hangman.py
@@ -2,12 +2,21 @@ import random
 wordBank = ["Violin", "Banjo", "Guitar", "Ukulele", "Drums", "Piano", "Accordion", "Flute", "Clarinet", "Trumpet"]
 Guesses = 8
 word = random.choice(wordBank)
-guess = input
+letters = []
 
 print("Welcome to Hangman!")
 print("Pick a letter")
 print(word)
 
-guess = input
-letters =
-letters.append(input)
+while Guesses > 0:
+    guess = input()
+    letters.append(guess)
+    print(letters)
+    Guesses -= 1
+    if guess in word:
+        print("You got one")
+    else:
+        Guesses -= 1
+        print("Wrong")
+    print(Guesses)
+
