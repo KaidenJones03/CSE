d7ae2c376328c5924705679b9744f7ba0cd38343
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-01-29 09:45:51 -0800

    Dictionary notes

---
+++ Dictionary notes.py
@@ -0,0 +1,24 @@
+states = {
+     "CA": "California",
+     "FL": "Florida",
+     "AK": "Alaska",
+     "GA": "Georgia",
+}
+
+print(states["CA"])
+print(states["AK"])
+
+nested_dictionary = {
+     "CA":""
+          "NAME": "California",
+          "POPULATION": 3950000   # 39,500,000
+
+},
+
+
+     "FL": "Florida",
+     "AK": "Alaska",
+     "GA": "Georgia",
+}
+
+
