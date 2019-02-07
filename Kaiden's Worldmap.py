90e11f8cc073c93e47937fbc1fa57386a605cf17
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-02-07 09:45:15 -0800

    Kaiden's Worldmap

--- Kaiden's Worldmap.py
+++ Kaiden's Worldmap.py
@@ -1,4 +1,21 @@
 world_map = {
     'Battle Bus': {
         'NAME': "Battle Bus",
-        'DESCRIPTION':"You are in the battle bus ""Now you can either go "
+        'DESCRIPTION':"You are in the battle bus "
+                      "Now you can either go Retail Row or Paradise Palms ",
+        'PATHS': {
+            'NORTH': "RETAIL ROW"
+        }
+    },
+    'RETAIL ROW': {
+        'NAME': "RETAIL ROw",
+        'DESCRIPTION': "You are at retail row. if you go south you will be at Paradise Palm"
+                       "If you go north you will reach tomato temple",
+        "PATHS": {
+            'SOUTH':''
+
+        }
+    },
+
+}
+
