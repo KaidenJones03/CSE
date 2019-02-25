29f0791839e660da57555d703dc5c23e8c96dfb2
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-02-25 09:45:50 -0800

    World Map OOP

--- Kaiden's Worldmap.py
+++ Kaiden's Worldmap.py
@@ -1,8 +1,8 @@
 world_map = {
     'Battle Bus': {
         'NAME': "Battle Bus",
-        'DESCRIPTION':"You are in the battle bus "
-                      "Now you can either go North to Retail Row or south to Paradise Palms ",
+        'DESCRIPTION': "You are in the battle bus "
+                       "Now you can either go North to Retail Row or south to Paradise Palms ",
         'PATHS': {
             'NORTH': "RETAIL ROW",
             'SOUTH': "PARADISE PALMS"
@@ -52,72 +52,72 @@ world_map = {
     },
     'PLEASANT PARK': {
         'NAME': "PLEASANT PARK",
-        'DESCRIPTION':"You are at Pleasant Park. This place is a nice park but isnt pleasant"
-                      "You can go North to junk junction, or south to viking mountain",
+        'DESCRIPTION': "You are at Pleasant Park. This place is a nice park but isnt pleasant"
+                       "You can go North to junk junction, or south to viking mountain",
         'PATHS': {
-            'NORTH':"JUNK JUNCTION",
-            'SOUTH':"VIKING MOUNTAIN"
+            'NORTH': "JUNK JUNCTION",
+            'SOUTH': "VIKING MOUNTAIN"
         }
     },
     'JUNK JUNCTION': {
-        'NAME':"JUNK JUNCTION",
-        'DESCRIPTION':"You are at junk junction.This place is a trash dump and smells really bad"
-                      "You can either go south to Pleasant park or east to lazy links",
+        'NAME': "JUNK JUNCTION",
+        'DESCRIPTION': "You are at junk junction.This place is a trash dump and smells really bad"
+                       "You can either go south to Pleasant park or east to lazy links",
         'PATHS': {
-            'SOUTH':"PLEASANT PARK",
-            'EAST':"LAZY LINKS"
+            'SOUTH': "PLEASANT PARK",
+            'EAST': "LAZY LINKS"
         }
     },
     'THE BLOCK': {
-        'NAME':"THE BLOCK",
-        'DESCRIPTION':"You are at the block.This place is updated every week with player creations"
-                      "You can go south to Wailing Woods,or west to lazy links",
+        'NAME': "THE BLOCK",
+        'DESCRIPTION': "You are at the block.This place is updated every week with player creations"
+                       "You can go south to Wailing Woods,or west to lazy links",
         'PATHS': {
-            'SOUTH':"WAILING WOODS",
-            'WEST':"LAZY LINKS"
+            'SOUTH': "WAILING WOODS",
+            'WEST': "LAZY LINKS"
         }
     },
     'WAILING WOODS': {
-        'NAME':"WAILING WOODS",
+        'NAME': "WAILING WOODS",
         'DESCRIPTION': "You are at Wailing Woods.This place is a calm forest"
                        "You can go south to retail row or west to tomato temple",
         'PATHS': {
-            'SOUTH':"RETAIL ROW",
-            'WEST':"TOMATO TEMPLE"
+            'SOUTH': "RETAIL ROW",
+            'WEST': "TOMATO TEMPLE"
         }
     },
     'FATAL FIELDS': {
-        'NAME':"FATAL FIELDS",
+        'NAME': "FATAL FIELDS",
         'DESCRIPTION': "You are at Fatal FIelds. This place is a nice little farm ranch"
                        "You can west to happy hamlet or north to salty springs",
         'PATHS': {
-            'WEST':"HAPPY HAMLET",
-            'NORTH':"SALTY SPRINGS"
+            'WEST': "HAPPY HAMLET",
+            'NORTH': "SALTY SPRINGS"
             }
     },
     'HAPPY HAMLET': {
-        'NAME':"HAPPY HAMLET",
-        'DESCRIPTION':"You are at happy hamlet. Its really cold here but there is a small town"
-                      "You can either go west to frosty flights or north to tilted towers",
+        'NAME': "HAPPY HAMLET",
+        'DESCRIPTION': "You are at happy hamlet. Its really cold here but there is a small town"
+                       "You can either go west to frosty flights or north to tilted towers",
         'PATHS': {
-            'WEST':"FROSTY FLIGHTS",
-            'NORTH':"TILTED TOWERS"
+            'WEST': "FROSTY FLIGHTS",
+            'NORTH': "TILTED TOWERS"
         }
     },
     'FROSTY FLIGHTS': {
-        'NAME':"FROSTY FLIGHTS",
-        'DESCRIPTION':"You are at frosty flights. There are a few airplane hangars"
-                      "You can either go north to viking mountain or east to happy hamlet",
+        'NAME': "FROSTY FLIGHTS",
+        'DESCRIPTION': "You are at frosty flights. There are a few airplane hangars"
+                       "You can either go north to viking mountain or east to happy hamlet",
         'PATHS': {
-            'EAST':"HAPPY HAMLET",
-            'NORTH':"VIKING MOUNTAIN"
+            'EAST': "HAPPY HAMLET",
+            'NORTH': "VIKING MOUNTAIN"
         }
     },
     'VIKING MOUNTAIN': {
         'NAME': "VIKING MOUNTAIN",
         'DESCRIPTION': "You are at Viking Mountain. This is a lost viking camp on a mountain"
-                      "You can either go east to tilted towers, west to snobby shores, north to pleasant park"
-                      "or south to frosty flights",
+                       "You can either go east to tilted towers, west to snobby shores, north to pleasant park"
+                       "or south to frosty flights",
         'PATHS': {
             'EAST': "TILTED TOWERS",
             'WEST': "SNOBBY SHORES",
@@ -127,7 +127,7 @@ world_map = {
     'TILTED TOWERS': {
         'NAME': "TILTED TOWERS",
         'DESCRIPTION': "You are at Tilted towers. this place is the biggest city on the map"
-                      "You can go north to pleasant park,west to viking mountain,or south to happy hamlet",
+                       "You can go north to pleasant park,west to viking mountain,or south to happy hamlet",
         'PATHS': {
             'NORTH':  "PLEASANT PARK",
             'WEST': "VIKING MOUNTAIN",
@@ -137,7 +137,7 @@ world_map = {
     'SNOBBY SHORES': {
         'NAME': "SNOBBY SHORES",
         'DESCRIPTION': "You are at Snobby Shores. This place has a few modern apartments"
-                      "you can either go east to viking mountain,south to frosty flights, or north to junk junction",
+                       "you can either go east to viking mountain,south to frosty flights, or north to junk junction",
         'PATHS': {
             'EAST': "VIKING MOUNTAIN",
             'SOUTH': "FROSTY FLIGHTS",
--- World Map OOP.py
+++ World Map OOP.py
@@ -1,17 +1,23 @@
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None):
+    def __init__(self, name, description="", north=None, south=None, east=None, west=None):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
+        self.west = west
+        self.description = description
 
 
-# Option 1 - define as we go
-R19A = Room("Mr. Wiebe's Room")
-parking_lot = Room("Parking Lot", None, R19A)
+R19A = Room("Mr. Wiebe's Room", "This is mr wiebe's room", 'parking_lot')
+parking_lot = Room("Parking Lot", "The parking lot outside", None, "R19A")
 
-R19A.north = parking_lot
+retail = Room("RETAIL ROW", "You are at retail row. if you go south you will be at Paradise Palm. \n"
+                            "If you go north you will reach tomato temple",
+              "tomato_temple", "palms")
+palms = Room("PARADISE PALMS", "You are at Paradise Palms. Its very dry here with a lot of mountains"
+                               "You can either go north to retail row or west to fatal fields",
+             "retail", "fatal")
+tomato = Room("TOMATO TEMPLE", "You are at Tomato Temple. This place was a religious sanctuary for tomatoes"
+                               "You can either go east to wailing woods, north to the block, or west to lazy links",
+              "THE BLOCK", None, "WAILING WOODS", "LAZY LINKS",)
 
-# Option 2 - Set all at once, modify controller
-R19A = Room("Mr. Wiebe's Room, 'parking_lot'")
-parking_lot = Room("Parking Lot", None, R19A)
