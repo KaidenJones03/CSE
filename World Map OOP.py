d7aadccc2e145961a212e85350feaa03858c7304
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-02-27 09:45:40 -0800

    World Map OOP

--- World Map OOP.py
+++ World Map OOP.py
@@ -8,40 +8,51 @@ class Room(object):
         self.description = description
 
 
-     class Player(object):
-         def __init__(self, starting_location):
-             self.current_location = starting_location
-             self.inventory = []
+class Player(object):
+    def __init__(self, starting_location):
+         self.current_location = starting_location
+         self.inventory = []
 
-         def move(self, new_location):
+    def move(self, new_location):
+        """
 
-        def move(self, new_location):
-            """
-
-            :param new_location: The room object of which you are
-            :return:
-            """
-            self.current_location = new_location
+        :param new_location: The room object of which you are
+        :return:
+        """
+        self.current_location = new_location
 
 
-def find_next_room(direction):
-    """This method searches the current room so ee if a room exists in that direction
+    def find_next_room(self, direction):
+        """This method searches the current room so ee if a room exists in that direction
 
-    :param direction: The direction that you want to move to
-    :return: The room object if it exists, or None if it doe
-    """
-    return getattr(self.current_location, direction)
-# option 1 define as we go
+        :param direction: The direction that you want to move to
+        :return: The room object if it exists, or None if it doe
+        """
+        name_of_room = getattr(self.current_location, direction)
+        return globals()[name_of_room]
+
+
 R19A = Room("Mr. Wiebe's Room", "This is mr wiebe's room", 'parking_lot')
 parking_lot = Room("Parking Lot", "The parking lot outside", None, "R19A")
 
-player = Player(R19A)
+
+retail = Room("RETAIL ROW", "You are at retail row. if you go south you will be at Paradise Palm. \n"
+                            "If you go north you will reach tomato temple",
+              "tomato", "palms")
+palms = Room("PARADISE PALMS", "You are at Paradise Palms. Its very dry here with a lot of mountains"
+                               "You can either go north to retail row or west to fatal fields",
+             "retail", "fatal")
+tomato = Room("TOMATO TEMPLE", "You are at Tomato Temple. This place was a religious sanctuary for tomatoes"
+                               "You can either go east to wailing woods, north to the block, or west to lazy links",
+              "THE BLOCK", None, "WAILING WOODS", "LAZY LINKS",)
+player = Player(retail)
+
 
 playing = True
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
 while playing:
-    print(Player.current_location.name)
-    print(current_location.description)
+    print(player.current_location.name)
+    print(player.current_location.description)
     command = input(">_")
     if command.lower() in ['q', 'quit' 'exit']:
         playing = False
@@ -51,23 +62,5 @@ while playing:
             player.move(next_room)
         except KeyError:
             print("I cant go that way")
-        else:
-            print(" Command not found")
-            room_name = current_node['PATHS'][command.upper()]
-            current_node = world_map[room_name]
-        except KeyError:
-            print("I cant go that way")
     else:
         print("Command Not Found")
-
-
-retail = Room("RETAIL ROW", "You are at retail row. if you go south you will be at Paradise Palm. \n"
-                            "If you go north you will reach tomato temple",
-              "tomato_temple", "palms")
-palms = Room("PARADISE PALMS", "You are at Paradise Palms. Its very dry here with a lot of mountains"
-                               "You can either go north to retail row or west to fatal fields",
-             "retail", "fatal")
-tomato = Room("TOMATO TEMPLE", "You are at Tomato Temple. This place was a religious sanctuary for tomatoes"
-                               "You can either go east to wailing woods, north to the block, or west to lazy links",
-              "THE BLOCK", None, "WAILING WOODS", "LAZY LINKS",)
-
