75e5b90366b478fda7034b44ada8b8bba882daf3
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-02-28 09:45:19 -0800

    Inheritance

---
+++ Inheritance.py
@@ -0,0 +1,47 @@
+class Vehicle(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Car(Vehicle):
+    def __init__(self, name, mileage):
+        super(Car, self).__init__(name)
+        self.engine_status = False
+        self.fuel = 100
+        self.mileage = mileage
+
+    def start_engine(self):
+        self.engine_status = True
+        print("You turn the key and the car turns on")
+
+    def move_forward(self):
+        self.fuel -= 1
+        print("The car moves forward")
+
+    def turn_left(self):
+        self.fuel -= 1
+        print("The car turns left")
+
+    def turn_off(self):
+        self.engine_status = False
+        print("You turn off the car")
+
+
+class Impala(Car):
+    def __init__(self):
+        super(Impala, self).__init__("Impala", 25)
+
+
+class Keylesscar(Car):
+    def __init__(self, name, mileage):
+        super(Keylesscar, self).__init__(name, mileage)
+
+    def start_engine(self):
+        self.engine_status = True
+        print
+Jacob_car = Impala()
+Jacob_car.start_engine()
+Jacob_car.move_forward()
+Jacob_car.turn_left()
+Jacob_car.move_forward()
+Jacob_car.turn_off()
