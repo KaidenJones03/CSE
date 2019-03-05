f1dd9acde6a299ba68af9041af20e4ed91c473c3
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-03-05 09:46:33 -0800

    items

--- items.py
+++ items.py
@@ -1,3 +1,7 @@
 class Item(object):
-    def __init__(self):
-        self.name =
+    def __init__(self, name):
+        self.name = name
+
+class Consumables(Item):
+    def __init__(self,minis)
+        super(Item, self).__init__(name)
