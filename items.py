ac64c8547f70b87ab3ebb9391ed8f292c489e2cf
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-03-06 09:45:44 -0800

    items.py

--- items.py
+++ items.py
@@ -2,6 +2,27 @@ class Item(object):
     def __init__(self, name):
         self.name = name
 
-class Consumables(Item):
-    def __init__(self,minis)
-        super(Item, self).__init__(name)
+
+class Consumable(Item):
+    def __init__(self, name, description, quantity=1):
+        super(Consumable, self).__init__(name)
+        self.description = description
+        self.quantity = quantity
+
+
+class Mini(Consumable):
+    def __init__(self):
+        super(Mini, self).__init__("Mini Shield", "A mini Shield increases your shield points by 25", 3)
+
+
+billy_bob_joe = Mini()
+print(billy_bob_joe.quantity)
+
+
+class Shield_Pot(Consumable):
+    def __init__(self):
+        super(Shield_Pot, self).__init__("Shield Potion", "A Shield Potion grants you 50 shield points", 1)
+        
+
+class Bandages(Consumable):
+
