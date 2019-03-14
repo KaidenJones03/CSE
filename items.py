1211e64293caef8722a366e889b734d43d8b311d
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-03-14 09:43:51 -0700

    items

--- items.py
+++ items.py
@@ -39,6 +39,11 @@ class SlurpJuice(Consumable):
         super(SlurpJuice, self).__init__("SlurpJuice", "SlurpJuice restores  25 shield point")
 
 
+class ChugJug(Consumable):
+    def __abs__(self):
+        super(ChugJug, self).__init__("ChugJug", "ChugJug restores all of your health and shield points")
+
+
 class Weapon(Item):
     def __init__(self, name, description, bullets, damage):
         super(Item, self).__init__(name)
@@ -56,7 +61,35 @@ class TacticalShotgun(Weapon):
 class AssaultRifle(Weapon):
     def __init__(self):
         super(AssaultRifle, self).__init__("Assault Rifle", "The Assault Rifle is a all around military gun", 30, 33)
-        
+
+
+class SMG(Weapon):
+    def __init__(self):
+        super(SMG, self).__init__("SMG", "The SMG is a medium damage fast firing weapon", 30, 24)
+
+
+class P90(Weapon):
+    def __init__(self):
+        super(P90, self).__init__("P90", "The P90 has medium damage, fastest firing gun in its class", 40, 21)
+
+
+class HeavySniper(Weapon):
+    def __init__(self):
+        super(HeavySniper, self).__init__("heavy Sniper", "The Heavy Sniper has the slowest fire rate, but most damage"
+                                                          "in its class", 1, 150)
+
+
+class SilencedSniper(Weapon):
+    def __init__(self):
+        super(SilencedSniper, self).__init__("Silenced Sniper", "The silenced sniper has the fastest reload speed, and "
+                                                                "enough damage to take out someone in 2 shots", 1, 105)
+
+
+class Scar(Weapon):
+    def __init__(self):
+        super(Scar, self).__init__("Scar", "The Scar is the best AR in its class, has a high damage too", 30, 36)
+
+
 
 class Character(object):
     def __init__(self, name, weapon):
