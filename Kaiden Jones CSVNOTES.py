14f5fc7fafd2beb055da4149be3d66f769ea9e56
Kaiden Jones 4911 <9x2g@fresnou.org>
2019-04-25 09:48:19 -0700

    KJ

--- Kaiden Jones CSVNOTES.py
+++ Kaiden Jones CSVNOTES.py
@@ -1,8 +1,16 @@
 import csv
 
 with open("Book1.csv") as old_csv:
-    reader = csv.reader(old_csv)
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
     for row in reader:
         old_number = row[0]
-        (old_number) = int(row[0]) + 1
-        print(old_number)
+        first_num = int(old_number[0])
+        if first_num % 2 == 0:
+            new_number = old_number + 1
+        row[0] = new_number
+        writer. writerow(row)
+
+print("OK")
+
