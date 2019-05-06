4c19b38b1b75760bfec00f750c6e898eefdfa9d0
KaidenJones03 <42385841+KaidenJones03@users.noreply.github.com>
2019-05-06 09:49:27 -0700

    Validate

--- notes/validator backup.py
+++ notes/validator backup.py
@@ -1,17 +1,18 @@
-def valid_card_number(num: str):
+def validate(num: str):
     if len(num) != 16:
         return False
-    print(valid_card_number("7174233103126850"))
+    last_digit = num[15]
 
 
 valid_card_number = "7174233103126850"
 
+print(validate(valid_card_number))
 
-def reverse_it(string):
-    print(string[0:0:-1])
-    reverse_it(valid_card_number)
-
-
-list_num = list(valid_card_number)
-for index in range(len(list_num)):
-    list_num[index] = int(list_num[index])
+# def reverse_it(string):
+#     print(string[0:0:-1])
+#     reverse_it(valid_card_number)
+#
+#
+# list_num = list(valid_card_number)
+# for index in range(len(list_num)):
+#     list_num[index] = int(list_num[index])
