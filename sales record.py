30e894a5f6333deb4dc245ab3a766e3f102bf263
KaidenJones03 <42385841+KaidenJones03@users.noreply.github.com>
2019-05-10 10:39:57 -0700

    sales record almost complete

--- sales record.py
+++ sales record.py
@@ -2,9 +2,49 @@ import csv
 
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
+    fruit_totals = 0
     for row in reader:
         thing = row[13]  # This is a string
-        print(thing)
-    item_type = row[2]
-    if item_type == "Fruits":
-        print(item_type)
+        # print(thing)
+        item_type = row[2]
+        if item_type == 'Fruits':
+            print(thing)
+            fruit_totals += float(thing)
+    print(fruit_totals)
+
+with open("Sales Records.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    clothes_totals = 0
+    for row in reader:
+        clothes_profit = row[13]  # This is a string
+        # print(thing)
+        item_type = row[2]
+        if item_type == 'Clothes':
+            print(clothes_profit)
+            clothes_totals += float(clothes_profit)
+    print(clothes_totals)
+
+with open("Sales Records.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    meat_totals = 0
+    for row in reader:
+        meat_profit = row[13]  # This is a string
+        # print(thing)
+        item_type = row[2]
+        if item_type == 'Meat':
+            print(meat_profit)
+            meat_totals += float(meat_profit)
+    print(meat_totals)
+
+with open("Sales Records.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    meat_totals = 0
+    for row in reader:
+        meat_profit = row[13]  # This is a string
+        # print(thing)
+        item_type = row[2]
+        if item_type == 'Meat':
+            print(meat_profit)
+            meat_totals += float(meat_profit)
+    print(meat_totals)
+
