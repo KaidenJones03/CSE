e4c2ccf96a6f405118d4102adea3e55c7dfe7efa
KaidenJones03 <42385841+KaidenJones03@users.noreply.github.com>
2019-05-13 09:47:50 -0700

    sALE profits basically done

--- notes/validator backup.py
+++ notes/validator backup.py
@@ -16,3 +16,26 @@ print(validate(valid_card_number))
 # list_num = list(valid_card_number)
 # for index in range(len(list_num)):
 #     list_num[index] = int(list_num[index])
+with open("Sales Records.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    baby_food_totals = 0
+    for row in reader:
+        baby_food_profit = row[13]  # This is a string
+        # print(thing)
+        item_type = row[2]
+        if item_type == 'Baby Food':
+            print(baby_food_profit)
+            baby_food_totals += float(baby_food_profit)
+    print(baby_food_totals)
+
+    with open("Sales Records.csv", 'r') as old_csv:
+        reader = csv.reader(old_csv)
+        office_supplies_totals = 0
+        for row in reader:
+            office_supplies_profit = row[13]  # This is a string
+            # print(thing)
+            item_type = row[2]
+            if item_type == 'Office Supplies':
+                print(office_supplies_profit)
+                office_supplies_totals += float(office_supplies_profit)
+        print(office_supplies_totals)
--- sales record.py
+++ sales record.py
@@ -3,48 +3,62 @@ import csv
 with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     fruit_totals = 0
+    clothes_totals = 0
+    meat_totals = 0
+    beverage_totals = 0
+    baby_food_totals = 0
+    office_supplies_totals = 0
+    Cosmetics_totals = 0
+    Household_totals = 0
+    Cereal_totals = 0
+    Personal_Care_totals = 0
+    Vegetables_totals = 0
+    Snacks_totals = 0
     for row in reader:
-        thing = row[13]  # This is a string
+        profit = row[13]  # This is a string
         # print(thing)
         item_type = row[2]
         if item_type == 'Fruits':
-            print(thing)
-            fruit_totals += float(thing)
-    print(fruit_totals)
-
-with open("Sales Records.csv", 'r') as old_csv:
-    reader = csv.reader(old_csv)
-    clothes_totals = 0
-    for row in reader:
-        clothes_profit = row[13]  # This is a string
-        # print(thing)
-        item_type = row[2]
+            fruit_totals += float(profit)
         if item_type == 'Clothes':
-            print(clothes_profit)
-            clothes_totals += float(clothes_profit)
+            clothes_totals += float(profit)
+        if item_type == 'Meat':
+            meat_totals += float(profit)
+        if item_type == 'Beverage':
+            beverage_totals += float(profit)
+        if item_type == 'Baby Food':
+            baby_food_totals += float(profit)
+        if item_type == 'Office Supplies':
+            office_supplies_totals += float(profit)
+        if item_type == 'Cosmetics':
+            Cosmetics_totals += float(profit)
+        if item_type == 'HouseHold':
+            Household_totals += float(profit)
+        if item_type == 'Cereal':
+            Cereal_totals += float(profit)
+        if item_type == 'Personal Care':
+            Personal_Care_totals += float(profit)
+        if item_type == 'Vegetables':
+            Vegetables_totals += float(profit)
+        if item_type == 'Snacks':
+            Snacks_totals += float(profit)
     print(clothes_totals)
-
-with open("Sales Records.csv", 'r') as old_csv:
-    reader = csv.reader(old_csv)
-    meat_totals = 0
-    for row in reader:
-        meat_profit = row[13]  # This is a string
-        # print(thing)
-        item_type = row[2]
-        if item_type == 'Meat':
-            print(meat_profit)
-            meat_totals += float(meat_profit)
+    print(fruit_totals)
     print(meat_totals)
+    print(beverage_totals)
+    print(baby_food_totals)
+    print(office_supplies_totals)
+    print(Cosmetics_totals)
+    print(Household_totals)
+    print(Cereal_totals)
+    print(Personal_Care_totals)
+    print(Vegetables_totals)
+    print(Snacks_totals)
 
-with open("Sales Records.csv", 'r') as old_csv:
-    reader = csv.reader(old_csv)
-    meat_totals = 0
-    for row in reader:
-        meat_profit = row[13]  # This is a string
-        # print(thing)
-        item_type = row[2]
-        if item_type == 'Meat':
-            print(meat_profit)
-            meat_totals += float(meat_profit)
-    print(meat_totals)
+
+
+
+
+
+
 
