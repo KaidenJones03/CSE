cfb1332fcefa1f8cc23567fc8502763060f0efd5
KaidenJones03 <42385841+KaidenJones03@users.noreply.github.com>
2019-05-15 09:48:23 -0700

    sales rekords komplete

--- sales record.py
+++ sales record.py
@@ -6,16 +6,25 @@ with open("Sales Records.csv", 'r') as old_csv:
     clothes_totals = 0
     meat_totals = 0
     beverage_totals = 0
-    baby_food_totals = 0
     office_supplies_totals = 0
     Cosmetics_totals = 0
+    baby_food_totals = 0
     Household_totals = 0
     Cereal_totals = 0
     Personal_Care_totals = 0
     Vegetables_totals = 0
     Snacks_totals = 0
+    sub_saharan_africa_total = 0
+    middle_east_and_north_africa_total = 0
+    australia_and_oceania_total = 0
+    europe_total = 0
+    asia_total = 0
+    central_america_and_the_caribbean_total = 0
+    north_america_total = 0
     for row in reader:
-        profit = row[13]  # This is a string
+        profit = row[13]
+        region = row[0]
+        # This is a string
         # print(thing)
         item_type = row[2]
         if item_type == 'Fruits':
@@ -42,6 +51,21 @@ with open("Sales Records.csv", 'r') as old_csv:
             Vegetables_totals += float(profit)
         if item_type == 'Snacks':
             Snacks_totals += float(profit)
+        if region == 'Sub-Saharan Africa':
+            sub_saharan_africa_total += float(profit)
+        if region == 'Middle East and North Africa ':
+            middle_east_and_north_africa_total += float(profit)
+        if region == 'Australia and Oceania':
+            australia_and_oceania_total += float(profit)
+        if region == 'Europe':
+            europe_total += float(profit)
+        if region == 'Asia':
+            asia_total += float(profit)
+        if region == 'Central America and the Caribbean':
+            central_america_and_the_caribbean_total += float(profit)
+        if region == 'North America':
+            north_america_total += float(profit)
+
     print("clothes profit:", clothes_totals)
     print("fruit profit:", fruit_totals)
     print("meat profit:", meat_totals)
@@ -54,6 +78,13 @@ with open("Sales Records.csv", 'r') as old_csv:
     print("personal care profit:", Personal_Care_totals)
     print("vegetable:", Vegetables_totals)
     print("snacks profit:", Snacks_totals)
+    print("Sub-Saharan Africa profit:", sub_saharan_africa_total)
+    print("Middle East and North Africa", middle_east_and_north_africa_total)
+    print("Australia and Oceania:", australia_and_oceania_total)
+    print("Europe:", europe_total)
+    print("Asia:", asia_total)
+    print("Central America and the Caribbean:", central_america_and_the_caribbean_total)
+    print("North America:", north_america_total)
 
 
 list_of_profits = [clothes_totals, fruit_totals, meat_totals, beverage_totals, baby_food_totals, office_supplies_totals
